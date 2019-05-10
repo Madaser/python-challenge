e0791089251836174b358ec42046ad4ae690746c
madaser saleem <madasersaleem@gmail.com>
2019-05-09 21:20:14 -0400

    finished hw part 1

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,7 +4,6 @@ import csv
 sum=0
 csvpath=os.path.join('..', 'budget_data.csv')
 total= 0
-sum=0
 rows=[]
 
 with open(csvpath, newline='') as csvfile:
@@ -16,15 +15,91 @@ with open(csvpath, newline='') as csvfile:
 
  
     for row in csvreader:
-        print(row)
-        
         total=total+len(row)
         sum=sum+int(row[1])
         rows.append(row)
-    print(total/2)
-    print(sum)
+    Months=len(rows)
     Last_Value=int(row[1])
-    print(Last_Value)
+    
+    extrarows=[]
+    extracolumns=[]
+    for i in range(len(rows)):
+        extrarows.append((rows[i][1]))
+        extracolumns.append((rows[i][0]))
+        
+
+    
+    First_Value=int(extrarows[0])
+    Average=(Last_Value-First_Value)/Months
+    
+
+    g=0
+    best="month"
+    update="newm"
+    listnumber=range(len(extrarows))
+    for i in (range(len(listnumber)-1)):
+        if((int(extrarows[i+1])-int(extrarows[i])>0)):
+            m=(int(extrarows[i+1])-int(extrarows[i]))
+            best=extracolumns[i+1]
+            if(g>m):
+                m=g
+                best=update
+            else:
+                m=m
+                best=best    
+        else:
+            g=m
+            update=best
+    
+    
+
+    b=0
+    n=0
+    besty="month"
+    updatey="newm"
+    for i in (range(len(listnumber)-1)):
+        if((int(extrarows[i+1])-int(extrarows[i])<0)):    
+            b=(int(extrarows[i+1])-int(extrarows[i]))
+            besty=extracolumns[i+1]
+            if(n<b):
+                b=n
+                besty=updatey
+            else:
+                b=b
+                besty=besty
+        else:
+            n=b
+            updatey=besty
+    
+            
+
+
+    print(f'Total Months: {Months}')
+    print(f'Total Profits/Losses: {sum}')
+    print(f'Average Change: {Average}')
+    print(f'Greatest increase in profits: {best} ({m})')
+    print(f'Greatest decrease in profits: {besty} ({n})')
+
+    
+
+    output_path = os.path.join("..", "new.txt")
+    with open(output_path, 'w', newline='') as csvfile:
+
+    # Initialize csv.writer
+        csvwriter = csv.writer(csvfile, delimiter=',')
+
+        csvwriter.writerow([f'Total Months: {Months}'])
+        csvwriter.writerow([f'Total Profits/Losses: {sum}'])
+        csvwriter.writerow([f'Average Change: {Average}'])
+        csvwriter.writerow([f'Greatest increase in profits: {best} ({m})'])
+        csvwriter.writerow([f'Greatest decrease in profits: {besty} ({n})'])
+    
+        
+        
+
+
+
+        
         
 
 
